388860c6e52ea916a06e72bde6d05c652edab17b
Jessica Liang <jessicursliang@gmail.com>
2020-04-23 14:52:15 -0700

    generate cards using API

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
@@ -7,23 +7,46 @@ import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
-const Board = () => {
-  const boardComponents = CARD_DATA.cards.map((card, i) => {
-    return (
-      <li key={i}>
-        <Card 
-          text={card.text} 
-          emoji={card.emoji} 
-        />
-      </li>
-    );
-  });
+const Board = (props) => {
+  // const boardComponents = CARD_DATA.cards.map((card, i) => {
+  //   return (
+  //     <li key={i}>
+  //       <Card 
+  //         text={card.text} 
+  //         emoji={card.emoji} 
+  //       />
+  //     </li>
+  //   );
+  // });
+
+  const [cardList, setCardList] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const newCardList = [];
+
+  useEffect(() => {
+    axios.get(props.url + props.boardName + "/cards")
+    .then( (response) => {
+      for (let card of response.data) {
+        newCardList.push(<Card
+          key={card.card.id}
+          text={card.card.text}
+          emoji={card.card.emoji}
+        />);
+      };
+      
+      setCardList(newCardList);
+    })
+    .catch((error) => {
+      setErrorMessage(error.message);
+      console.log(error.message);
+    }) 
+  })
 
   return (
     <div>
-      { boardComponents }
+      { cardList }
     </div>
-  )
+  );
 };
 Board.propTypes = {
   
