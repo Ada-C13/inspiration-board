fb0453d95efd4aaa49ef778ad92cd7b0b3d58726
Maryam Heshmati <mairheshmati@gmail.com>
2020-04-23 16:25:48 -0700

    Added render  cards to board

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,24 +8,32 @@ import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
 const Board = (props) => {
-  
+  const cards = CARD_DATA.cards.map((card) => {
+    console.log(card);
 
-  // const API_URL_BASE = props.url
+    if(card['emoji'] && card['text']) {
+      return <li> <Card text={card.text} emoji={card.emoji}/> </li>
+    }else if(card['text']) {
+      return <li> <Card text={card.text}/> </li>
+    }else{
+      return <li> <Card emoji={card.emoji}/> </li>
+    }
+  });
+
+  return(
+  <div>Board
+    {cards}
+  </div>
+  );
+  
+};
+Board.propTypes = {};
+
+export default Board;
+ // const API_URL_BASE = props.url
 
   // const [cardList, setCards] = useState([])
 
   // useEffect(() => {
   //   return `${API_URL_BASE}${props.boardName}`
   // }, []);
-
-  return (
-    <div>
-      Board
-    </div>
-  )
-};
-Board.propTypes = {
-
-};
-
-export default Board;
