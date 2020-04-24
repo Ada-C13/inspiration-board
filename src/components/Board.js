3d791495e33f5cbc3cbaac3a7d8e4e09315870cc
ChelseaC13 <chelhelg@outlook.com>
2020-04-24 10:51:52 -0700

    emojis and clean code wave 2 done

--- src/components/Board.js
+++ src/components/Board.js
@@ -9,40 +9,23 @@ import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
 
-// const cardSamples = (data) => {
-//   console.log(`You have reached cardSamples`)
-//   console.log(data)
-//   const mappedCards = data.card.map((card) => {
-//     console.log("you have reached the mapping thing")
-//     return <Card text={card.text} emoji={card.emoji} />
-//   })
-//   console.log(`You have reached mappedCards ${mappedCards}`)
-//   return mappedCards
-// }
-
 const Board = ({url, boardName}) => {
   const [cardList, setCardList] = useState([])
   
   useEffect(() => {
   axios.get(`${url}${boardName}/cards`)
     .then ((response) => {
-      console.log(response.data)
       const apiData = response.data
-      console.log(apiData)
-      // setCardList(cardSamples(apiData))
       const mappedCards = apiData.map((card) => {
-        console.log("you have reached the mapping thing")
         return <Card text={card.card.text} emoji={card.card.emoji} />
-      })
-      console.log(mappedCards);      
+      })   
       setCardList(mappedCards);
-      // return cardSamples(response.data.card);
     })
     .catch((error) => {
-      // Still need to handle errors
+      
     });
   }, []);
-  // const cardList = cardSamples(CARD_DATA);
+
   
   return (
     <div className="board">
@@ -56,6 +39,3 @@ Board.propTypes = {
 
 export default Board;
 
-
-// const studentDataString = localStorage.getItem('studenList')
-  // const studentData = JSON.parse(studentDataString)
--- src/components/Card.js
+++ src/components/Card.js
@@ -5,17 +5,16 @@ import emojis from 'emoji-dictionary';
 import './Card.css';
 
 const Card = ({text, emoji}) => {
-  console.log(emoji)
-  console.log(text)
+
   return (
     <div className="card card__content">
       
       <p className="card__content-text">{text}</p>
       <p className="card__content-emoji" >{emojis.getUnicode(`${emoji}`)}</p>
-
+      
     </div>
   )
-
+}
 
 Card.propTypes = {
   text: PropTypes.string,
