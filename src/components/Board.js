a1c1691bbd886448d3fc6615f5bd0ca2f33717f3
Lola Llanes <mslourdesllanes@gmail.com>
2020-04-23 17:26:12 -0700

    added link for axios, need to work on state

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,13 +8,33 @@ import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
 const Board = () => {
+
+  let state = {
+    cards: cards,
+    cards: []
+  };
+
+
+  const API_URL = "https://inspiration-board.herokuapp.com/boards/Ada-Lovelace/cards"
+  axios.get(API_URL)  
+  .then ((response) => {
+    
+    cards:response.data
+
+    }
+  
   return (
     <div>
       < Card text="dummy text" />
     </div>
   )
 };
+
+
+
+
 Board.propTypes = {
+
 
 
 };
