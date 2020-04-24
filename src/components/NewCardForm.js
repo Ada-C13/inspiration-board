13a7bb9e734fcffd76dcdd2cef4e94639ce27cd6
Sharon Cheung <keikei1128@g.ucla.edu>
2020-04-24 09:45:04 -0700

    minior change

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -7,20 +7,21 @@ const EMOJI_LIST = ["", "heart_eyes", "beer", "clap", "sparkling_heart", "heart_
 
 const NewCardForm = () => {
   return (
-    <div className="new-card-form">
+    <form className="new-card-form">
       <section className="new-card-form__form">
         <header className="new-card-form__header">Add a new card</header>
-        <label className="new-card-form__form-label">Type an inspiration text here</label>
+        <div className="new-card-form__form-label">Type an inspiration text here</div>
+        <input className="new-card-form__form-textarea"></input>
+        <div className="new-card-form__form-label">Add an Emoji</div>
         <select className="new-card-form__form-select">
           <option value="heart_eyes">heart_eyes</option>
         </select>
-        <input className="new-card-form__form-textarea"></input>
-        <button className="new-card-form__form-button">Submit</button>
+        <button type='submit' className="new-card-form__form-button">Submit</button>
 
       </section>
       
 
-    </div>     
+    </form>     
   )
 
 }
