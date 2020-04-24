2db05e1eb3ba4f9a71f19e09c4b25840c4c964e8
quinqu <quinruby0@gmail.com>
2020-04-23 17:00:33 -0700

    newCardForm

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -4,3 +4,62 @@ import emoji from 'emoji-dictionary';
 import './NewCardForm.css';
 
 const EMOJI_LIST = ["", "heart_eyes", "beer", "clap", "sparkling_heart", "heart_eyes_cat", "dog"]
+
+
+const NewCardForm = () =>{
+
+    const [input, changeInput] = useState({
+        text: '', 
+        emoji: '',
+    })
+
+    const onInput = (event) => {
+        const newInput = {
+          ...input,
+        }
+        newInput[event.target.name] = event.target.value;
+        changeInput(newInput);
+    }
+
+
+    const onAddCard = (event) => {
+
+      event.preventDefault();
+
+        //create a function that calls back to game and adds the card
+
+      props.addCardCallback(input);
+      changeInput({
+        text: '', 
+        emoji: '',
+      })
+
+
+
+    }
+
+    return (
+        <div className='new-card-form'> 
+                <form onClick={onAddCard} > 
+                    <header> Add A Card </header>
+                    <label htmlFor='text' className='new-card-form__form-label'> Text </label>
+                    <input className='text' />
+
+                    <label htmlFor='emoji'  className='new-card-form__form-label'> Emoji </label>
+                    <input className='emoji' /> 
+
+
+                </form>
+
+
+                <button type='submit' className='new-card-form__form-button'>
+                    Submit Card
+                </button>
+
+        </div>
+
+
+    );
+}
+
+export default NewCardForm; 
