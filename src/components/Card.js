3cad09904f3a9d6afaf2fd5888e297a86f73f96b
Lola Llanes <mslourdesllanes@gmail.com>
2020-04-23 17:02:59 -0700

    passed in props; to add text to board

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,16 +4,21 @@ import emoji from 'emoji-dictionary';
 
 import './Card.css';
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="card">
       Card
+      <p> {props.text}</p>
+      {
+
+      }
+
     </div>
   )
 }
 
 Card.propTypes = {
-
+  text:PropTypes.string
 };
 
 export default Card;
