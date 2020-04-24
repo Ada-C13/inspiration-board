b4846f81382b56ab89828884340c85cfe52f4fa5
denisseai <msdenisseanaya@gmail.com>
2020-04-23 20:34:02 -0700

    Added spaces for reading

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,32 +8,25 @@ import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
 const Board = (props) => {
+
   const cards = CARD_DATA.cards.map((card) => {
-    console.log(card);
 
     if(card['emoji'] && card['text']) {
       return <p> <Card text={card.text} emoji={card.emoji}/> </p>
-    }else if(card['text']) {
+    }else if (card['text']) {
       return <p> <Card text={card.text}/> </p>
     }else{
       return <p> <Card emoji={card.emoji}/> </p>
     }
   });
 
-  return(
-  <div>Board
-    {cards}
-  </div>
+  return (
+    <div>
+      <p>{ cards }</p>
+    </div>
   );
   
 };
 Board.propTypes = {};
 
 export default Board;
- // const API_URL_BASE = props.url
-
-  // const [cardList, setCards] = useState([])
-
-  // useEffect(() => {
-  //   return `${API_URL_BASE}${props.boardName}`
-  // }, []);
