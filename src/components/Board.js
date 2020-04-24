ac85e88be7fb5e6d0437bbf6e06443f40d905550
Vera <lizeth.veraro@outlook.com>
2020-04-24 13:23:10 -0700

    create cardList to render each card

--- src/components/Board.js
+++ src/components/Board.js
@@ -8,9 +8,25 @@ import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
 const Board = () => {
+
+  const cardList = CARD_DATA.cards.map((card, i)=> {
+    console.log(i);
+    console.log(card);
+    return(
+      <Card
+        key={i}
+        // ? Why we cannot use key as a prop inside of card?
+        id={i}
+        text={card.text}
+        emoji={card.emoji}
+      />
+    );
+  })
+
   return (
     <div>
       Board
+      {cardList}
     </div>
   )
 };
