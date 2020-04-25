7d13038cba588a5855a21526c506489e7ce5ed9b
Rosalyn <syrosalynyu@gmail.com>
2020-04-25 14:39:55 -0700

    cleaned up Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import emoji from 'emoji-dictionary';
 import './Card.css';
 
 const Card = (props) => {
   
-
-  if (props.emoji === undefined) {
-    // console.log(props.text)
-  }
-  
   const onDeleteButtonClick = () => { 
     props.onDeleteCallback(props.id)
   }
-
-  // handleClick() {
-    
-  // }
 
   return (
     <div className="card__container">
       <div className="card">
         <div className="card__content">
-          <p className="card__content-text">Message: {props.text}</p>
-          <p className="card__content-emoji">Emoji:{emoji.getUnicode(`${props.emoji}`)}</p>
+          <p className="card__content-text">{props.text}</p>
+          <p className="card__content-emoji">{emoji.getUnicode(`${props.emoji}`)}</p>
         </div>
         <div className="card__delete">
             <button onClick={onDeleteButtonClick}>Delete</button> {/* Revisit later to see if CSS still functional/ optimal */ }
