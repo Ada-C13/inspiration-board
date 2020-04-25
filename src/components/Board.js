a6a9a70a3cf14efee68f5ccf8bc0cc87ecae3bfc
stpatrickschild <hannahtekie@gmail.com>
2020-04-25 11:06:33 -0400

    finished wave1 - sortof

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,10 +7,20 @@ import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
-const Board = () => {
+const Board = (props) => {
+  const cards = [
+      {id: 1, text: "you are cool", emoji: "flower"},
+      {id: 2, text: "you are doing enough" , emoji: "smile" },
+      {id: 3, text: "atleast it's not rainning", emoji: "coffee"},
+    ]
+    const cardComponents = cards.map((card) => {
+      return ( <Card id={card.id} text={card.text} emoji={card.emoji} />);
+    });
+  
   return (
-    <div>
-      Board
+    <div className="board">
+     {cardComponents}
+
     </div>
   )
 };
