5679d10970995d89b71eedce59c271325d43b609
Haben <haben7@gmail.com>
2020-04-24 17:43:37 -0700

    added test for NewFCardForm

---
+++ src/components/NewCardForm.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react'
+import NewCardForm from './NewCardForm';
+
+describe('NewCardForm', () => {
+  test('that it matches the existing snapshot', () => {
+    // Arrange-Act
+    const text = "hello";
+    const { asFragment } = render(
+      <NewCardForm newCardInfo={{text}} />
+      
+    );
+
+    // Assert
+    expect(asFragment()).toMatchSnapshot();
+    cleanup();
+  });
+});
