7d51399aff2ba5e6abd965f23c3746eaa1c8df5a
stpatrickschild <hannahtekie@gmail.com>
2020-04-25 19:29:14 -0400

    created initial new card form

--- src/components/NewCardForm.js
+++ src/components/NewCardForm.js
@@ -4,3 +4,26 @@ import emoji from 'emoji-dictionary';
 import './NewCardForm.css';
 
 const EMOJI_LIST = ["", "heart_eyes", "beer", "clap", "sparkling_heart", "heart_eyes_cat", "dog"]
+
+const NewCardForm = () => {
+  
+
+
+  return (
+    <div className="new-card-form">
+      <h1 className="new-card-form__header">
+        Making a new card!
+      </h1>
+      <form className="new-card-form__form">
+        <div>
+          <label className="new-card-form__form-label"></label>
+          <input 
+            type="text" 
+            className="new-card-form__form-textarea"/>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default NewCardForm;
