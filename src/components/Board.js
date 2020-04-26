ef8914b0f7711c91e0b7527f8612e59456bfe7ba
Lola Llanes <mslourdesllanes@gmail.com>
2020-04-26 13:22:00 -0700

    wave 2: modified/added axios to retrieve card

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
@@ -6,37 +6,70 @@ import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
+// props: URL or boardname
 
-const Board = () => {
+const Board = (props) => {
+  const API_CARD_URL = `${props.url}${props.bordName}/cards`
+  const [cards, setCardList] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null);
 
-  let state = {
-    cards: cards,
-    cards: []
-  };
-
-
-  const API_URL = "https://inspiration-board.herokuapp.com/boards/Ada-Lovelace/cards"
-  axios.get(API_URL)  
-  .then ((response) => {
-    
-    cards:response.data
-
-    }
+  //load cards
+  //Source: https://github.com/Ada-Developers-Academy/textbook-curriculum/blob/master/React/apis-get.md
+  useEffect(() => {
+    axios.get(API_CARD_URL)
+      .then((response) => {
+        const apiData = response.data; //an array of all cards (objects)
+        const cardAPI = apiData.map((card) => {
+          return {
+            id: card.card.id,
+            text: card.card.text,
+            emoji: card.card.emoji,
+          };
+        });
+        setCardList(cardAPI); //updating the cards list with the response from the API 
+      })
+      .catch((error) => {
+        errorMessage(error.message);
+      });
+  }, []);
   
-  return (
-    <div>
-      < Card text="dummy text" />
-    </div>
-  )
+
+// Source: https://github.com/AdaGold/inspiration-board-api
+const removeCard = (id) => {
+  axios
+    .delete(`https://inspiration-board.herokuapp.com/cards/${id}`)
+    .then((response) => {
+      const updatedCards = cards.filter(
+        (card) => card.id !== response.data.card.id 
+      );
+      setCardList(updatedCards); 
+    })
+    .catch((error) => {
+      setErrorMessage(error.message);
+    });
 };
 
+  // let state = {
+  //   cards: cards,
+  //   cards: []
+  // };
+
+  // const API_URL = "https://inspiration-board.herokuapp.com/boards/Ada-Lovelace/cards"
+  // axios.get(API_URL)  
+  // .then ((response) => {
+    
+  //   cards:response.data
+    
+  //   },
+
+  
+
+
+// Board.propTypes = {
 
 
 
-Board.propTypes = {
-
-
-
-};
+// };
+}
 
 export default Board;
