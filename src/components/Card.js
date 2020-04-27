d5bf7f50343941908754395a737e543bf7832ee1
Yaz <yasmineoshaugh@gmail.com>
2020-04-26 17:49:25 -0700

    added Delete button, defined proptypes

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,19 +11,29 @@ const validateEmoji = (emojiText)=> {
 
 const Card = (props) => {
 
+  const deleteCard = (event) => {
+    event.preventDefault();
+    props.deleteCallBack(props.id);
+  };
+
   console.log(props);
   return (
     <div id={props.id} className="card">
       <div className="card__content">
         <p className="card__content-text">{props.text} </p>
         <p className="card__content-emoji">{validateEmoji(props.emoji)} </p>
+        <button className="card__delete" onClick={deleteCard}>
+          Delete
+        </button>
       </div>
     </div>
   )
 }
 
 Card.propTypes = {
-
+  text: PropTypes.string,
+  emoji: PropTypes.string,
+  deleteCallBack: PropTypes.func.isRequired,
 };
 
 export default Card;
