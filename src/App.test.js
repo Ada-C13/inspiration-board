9d6f4cd8efc71b314080751138062b7f8c9c974c
Vera <lizeth.veraro@outlook.com>
2020-04-27 16:01:21 -0700

    Adding test

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
 describe('App', () => {
 
-  it('renders without crashing', () => {
-    console.log('testing would be nice! :)');
+  // it('renders without crashing', () => {
+  //   console.log('testing would be nice! :)');
+  // });
+
+  test('Renders Board', () => {
+    const { getByText } = render(<App />);
+    const element = getByText(/Board/i);
+    expect(element).toBeInTheDocument();
   });
 
 });
+
