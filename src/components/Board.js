dfe121db8be9f23326bfdf6b0804d8653af6dfc8
Yaz <yasmineoshaugh@gmail.com>
2020-04-26 17:50:33 -0700

    started deleteCallBack func - successfully removed from board but doesn't yet successfully remove from API

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,11 +2,9 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
-
 import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
-import CARD_DATA from '../data/card-data.json';
 
 const reformatData = (data)=> {
   return data.map((element) => {
@@ -18,7 +16,7 @@ const Board = (props) => {
 
   const [cardList, setCardList] = useState([]);
 
-  const endPoint = `${props.url}/${props.boardName}`
+  const endPoint = `${props.url}${props.boardName}`
 
   const getCards = (url) => {
     axios.get(`${url}/cards`)
@@ -33,6 +31,26 @@ const Board = (props) => {
 
   useEffect(() => { getCards(endPoint); }, [endPoint] );
 
+  const deleteCallBack = (cardID) => {
+    const newCardList = [];
+    console.log(cardList);
+    cardList.forEach((card)=>{
+      if (card.id === cardID) {
+        // axios call to remove this card
+        axios.delete(`${endPoint}/cards/${card.id}`)
+          .then((response)=> {
+            console.log(`Card # ${cardID} deleted.`);
+          })
+          .catch((error)=>{
+            console.log(`Couldn't delete card # ${cardID}`);
+          });
+      } else {
+        newCardList.push(card);
+      }
+    });
+    return setCardList(newCardList);
+  };
+
   const formatCards = cardList.map((card) => {
 
     return (
@@ -41,18 +59,32 @@ const Board = (props) => {
         id={card.id}
         text={card.text}
         emoji={card.emoji}
+        deleteCallBack={deleteCallBack}
       />
     );
   });
 
+  //setCardList(formatCards);
+
+  const addCardCallBack = () => {
+    //const newCardList = []
+    return [];
+  };
+
   return (
-    <div className="board">
-      {formatCards}
-    </div>
-  )
+    <main>
+      <div className="board">
+        {formatCards}
+      </div>
+      <div>
+        <NewCardForm boardName={props.boardName} addCardCallBack={addCardCallBack}/>
+      </div>
+    </main>
+  );
 };
 Board.propTypes = {
-
+  url: PropTypes.string.isRequired,
+  boardName: PropTypes.string.isRequired,
 };
 
 export default Board;
