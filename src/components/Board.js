27134ec69de1d0ce356f085ee181638d053ba31b
Yesenia <yesentorres@gmail.com>
2020-04-26 19:33:36 -0700

    refactored get request, created function to generate card components

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect, } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
-
 import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
-import CARD_DATA from '../data/card-data.json';
-// props: URL or boardname
+
+const emoji = require("emoji-dictionary");
 
 const Board = (props) => {
   const API_CARD_URL = `${props.url}${props.bordName}/cards`
@@ -15,24 +14,27 @@ const Board = (props) => {
 
   //load cards
   //Source: https://github.com/Ada-Developers-Academy/textbook-curriculum/blob/master/React/apis-get.md
-  useEffect(() => {
+  useEffect( () => {
     axios.get(API_CARD_URL)
       .then((response) => {
-        const apiData = response.data; //an array of all cards (objects)
-        const cardAPI = apiData.map((card) => {
-          return {
-            id: card.card.id,
-            text: card.card.text,
-            emoji: card.card.emoji,
-          };
-        });
-        setCardList(cardAPI); //updating the cards list with the response from the API 
+        const apiData = [...cards, ...response.data]; //a combined array of all cards (objects)
+        setCardList(apiData); //updating the cards list with the response from the API 
       })
       .catch((error) => {
-        errorMessage(error.message);
+        setErrorMessage(error.message);
       });
-  }, []);
-  
+    }, []);
+
+  // turn each card object into a card component
+  const cardComponents = cards.map((cardObj) => {
+    return(< Card 
+      id={cardObj.card.id}
+      text={cardObj.card.text} 
+      emoji={cardObj.card.emoji === null ? "" : emoji.getUnicode(cardObj.card.emoji)}
+      removeCardCallback={removeCard}
+      key={cardObj.card.id}
+    />)
+  })
 
 // Source: https://github.com/AdaGold/inspiration-board-api
 const removeCard = (id) => {
@@ -49,27 +51,33 @@ const removeCard = (id) => {
     });
 };
 
-  // let state = {
-  //   cards: cards,
-  //   cards: []
-  // };
-
-  // const API_URL = "https://inspiration-board.herokuapp.com/boards/Ada-Lovelace/cards"
-  // axios.get(API_URL)  
-  // .then ((response) => {
+return (
+  <div className="board">   
+    {errorMessage ? <div><h2>{errorMessage}</h2></div> : ''}
+    {cardComponents}
     
-  //   cards:response.data
-    
-  //   },
+  </div>
+)
 
-  
-
-
-// Board.propTypes = {
-
-
-
-// };
 }
 
 export default Board;
+
+// Before Refactoring:
+  // useEffect(() => {
+  //   axios.get(API_CARD_URL)
+  //     .then((response) => {
+  //       const apiData = response.data; //an array of all cards (objects)
+  //       const cardAPI = apiData.map((card) => {
+  //         return {
+  //           id: card.card.id,
+  //           text: card.card.text,
+  //           emoji: card.card.emoji,
+  //         };
+  //       });
+  //       setCardList(cardAPI); //updating the cards list with the response from the API 
+  //     })
+  //     .catch((error) => {
+  //       errorMessage(error.message);
+  //     });
+  // }, []);
